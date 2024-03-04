11afca7ec63b775e42e6f88a3b616b08a70fb4eb
KitYan20 <kityan3002@gmail.com>
2024-03-04 02:00:48 -0500

    working on *

--- a1/main.c
+++ a1/main.c
@@ -26,53 +26,58 @@ int main(int argc, char *argv[]){
     const char *pattern = NULL;
     int include_links = 0;
 
-    if (argc < 2){
-        fprintf(stderr,"Need to pass in arguments\n");
-        exit(EXIT_FAILURE);
-    }
-    while((opt = getopt(argc,argv, "p:f:s:l")) != -1){
-        switch(opt){
-            case 'p':
-                pathname = optarg;
-                break;
-            case 'f':
-                if (optarg[0] != 'c' && optarg[0] != 'h' && optarg[0] != 'S' && optarg[0] != 's'){
-                    fprintf(stderr,"Option -%c requires either c, h, s as argument for f\n",optopt);
-                    return 1;
-                }
-                file_type = optarg;
-                break;
-            case 's':
-                pattern = optarg;
-                break;
-            case 'l':
-                include_links = 1;
-                break;
-            case '?':
-                if (optopt == 'p'){
-                    fprintf(stderr,"Option -%c requires a pathname.\n",optopt);
-                    return 1;
-                }if (optopt = 's'){
-                    fprintf(stderr,"Option -%c requires a string to match.\n",optopt);
-                    return 1;
-                }if (optopt = 'f'){
-                    fprintf(stderr,"Option -%c requires file type.\n",optopt);
-                    return 1;
-                }else{
-                    fprintf (stderr,"Unknown option character `\\x%x'.\n",optopt);
-                    return 1;
-                }
-            default:
-                //abort();
-                exit(EXIT_FAILURE);
-        }
-    }
+    // if (argc < 2){
+    //     fprintf(stderr,"Need to pass in arguments\n");
+    //     exit(EXIT_FAILURE);
+    // }
+    // while((opt = getopt(argc,argv, "p:f:s:l")) != -1){
+    //     switch(opt){
+    //         case 'p':
+    //             pathname = optarg;
+    //             break;
+    //         case 'f':
+    //             if (optarg[0] != 'c' && optarg[0] != 'h' && optarg[0] != 'S' && optarg[0] != 's'){
+    //                 fprintf(stderr,"Option -%c requires either c, h, s as argument for f\n",optopt);
+    //                 return 1;
+    //             }
+    //             file_type = optarg;
+    //             break;
+    //         case 's':
+    //             pattern = optarg;
+    //             break;
+    //         case 'l':
+    //             include_links = 1;
+    //             break;
+    //         case '?':
+    //             if (optopt == 'p'){
+    //                 fprintf(stderr,"Option -%c requires a pathname.\n",optopt);
+    //                 return 1;
+    //             }if (optopt = 's'){
+    //                 fprintf(stderr,"Option -%c requires a string to match.\n",optopt);
+    //                 return 1;
+    //             }if (optopt = 'f'){
+    //                 fprintf(stderr,"Option -%c requires file type.\n",optopt);
+    //                 return 1;
+    //             }else{
+    //                 fprintf (stderr,"Unknown option character `\\x%x'.\n",optopt);
+    //                 return 1;
+    //             }
+    //         default:
+    //             //abort();
+    //             exit(EXIT_FAILURE);
+    //     }
+    // }
     
-    printf("finds -p %s -f %s -l %d -s %s\n",pathname,file_type,include_links,pattern);
-    myftw(pathname,pattern,file_type,include_links,myfunc);
-    // wildcard("a9b","a.b");
-    // wildcard("ab","a.b");
-    // wildcard("abb","a.b");
+    // printf("finds -p %s -f %s -l %d -s %s\n",pathname,file_type,include_links,pattern);
+    // myftw(pathname,pattern,file_type,include_links,myfunc);
+    wildcard("b","a*b");
+    wildcard("acbbb","a*b");
+    wildcard("abb","a*b");
+    wildcard("a","a*b");
+    wildcard("cbbb","a*b");
+    wildcard("accc","a*b");
+    wildcard("cccb","a*b");
+    wildcard("zzzzz","a*b");
     // wildcard("greatSentenceaXbcool","a.b");
     return 0;
 }
--- a1/wildcard.c
+++ a1/wildcard.c
@@ -6,10 +6,13 @@
 bool wildcard(const char* str, char* pattern){
     
     char ch = *pattern;
+    char pattern_copy[strlen(pattern)+1];
+    strcpy(pattern_copy,pattern);
+    //Make a copy of the pattern string
+    char str_copy[strlen(str)+1];
+    strcpy(str_copy,str);
     if (strstr(pattern,".") != NULL){
         //Copy the pattern string 
-        char pattern_copy[strlen(pattern)+1];
-        strcpy(pattern_copy,pattern);
         //printf("%s ",str);
         //Separate the pattern string into tokens by the "."
         char* token = strtok(pattern_copy,".");
@@ -26,9 +29,6 @@ bool wildcard(const char* str, char* pattern){
         char token3 = *token1;
         char token4 = *token2;
         int i = 0;
-        //Make a copy of the pattern string
-        char str_copy[strlen(str)+1];
-        strcpy(str_copy,str);
         while (str_copy[i] != '\0'){
             //Checks if the current character in the string matches the first character in the pattern string
             // This will be the intial state
@@ -73,8 +73,59 @@ bool wildcard(const char* str, char* pattern){
         //printf("No Pattern Found\n");
         return false;
     }else if (strstr(pattern,"*") != NULL) {
-        printf("Wildcard with *\n");
-        return true;
+        //Separate the pattern string into tokens by the "."
+        char* token = strtok(pattern_copy,"*");
+        //Get the token from the left side
+        char* token1 = token;
+        //Get the token from the right side
+        char *token2;
+        //Standard function to get the token
+        while (token != NULL) {
+            token2 = token;
+            token = strtok(NULL, "*");
+        }
+        //Dereference the first and second token into chars because 'C' 
+        char token3 = *token1;
+        char token4 = *token2;
+        int i = 0;
+        //Checks if the string is length one and the only character is not the next character
+        if (strlen(str) == 1 && str_copy[0] == token3){
+            printf("No pattern found\n");
+            return false;    
+        }
+        //The first character is the token
+        if (str_copy[i] == token4){
+            printf("Pattern Matched for %s\n",str_copy);
+            return true;
+        //Checks if there is previous character and the next character matches the token
+        }else if (str_copy[i] == token3 && str_copy[i+1] == token4 ){
+            printf("Pattern Matched\n");
+            return true;
+        //Checks if the previous character doesn't appear but the next character does match with the token
+        }else if (str_copy[i] != token3 && str_copy[i+1] == token4){
+            printf("Pattern Matched\n");
+            return true;
+        //Else case to handle longer strings
+        //1st case: if the previous character appears but the second one doesn't match
+        //2nd case: if the previous character doesn't appear and the next character doesn't either
+        }else if ((str_copy[i] == token3 && str_copy[i+1] != token4) || (str_copy[i] != token3 && str_copy[i+1] != token4)){
+            //printf("%c",str_copy[i]);
+            while(str_copy[i] != '\0'){
+                if (str_copy[i] == token4){
+                    printf("Pattern Matched\n");
+                    return true;
+                    break;
+                }else{
+                    i++;
+                }
+            }
+            printf("No pattern found\n");
+            return false;
+        }else{
+            printf("No pattern found");
+            return false;
+        }
+        
     }else if (strstr(pattern,"?") != NULL){
         printf("Wildcard with ?\n");
         return true;
